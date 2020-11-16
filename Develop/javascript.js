b020f233bf219e317639490a4f6696f2872c00ed
Irina Skitayeva <irina.skitayeva@gmail.com>
2020-11-15 20:26:51 -0500

    readme file

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -3,39 +3,17 @@
 
   date.textContent =moment().format("dddd, MMMM Do");  
 
-  $("textarea").on("click", function() {
-    
-//     var index = $(this)
-//     .closest(".list-group-item")
-//     .index();
-//     tasks[status][index].text = text;
-// saveTasks();
-});
+  
 
 
-  $("button").on("click", function() {
+  $("button").on("click", function(event) {
+     $(event.target)
     var textArea = $(this).closest(".row").find("textarea")
     textInput = textArea.val().trim();
+    
 
 
     localStorage.setItem("tasks", JSON.stringify(textInput));
 
 });
 
-
-var saveTasks= function(){
-tasks = JSON.parse(localStorage.getItem("tasks"));      
-        // if nothing in localStorage, create a new object to track all task status arrays
-        if (!tasks) {
-          tasks = {
-            task: [],
-                     };
-        }
-        $.each(tasks, function(list, arr) {
-            console.log(list, arr);
-            // then loop over sub-array
-           
-          });
-        };
-        
-        saveTasks();
